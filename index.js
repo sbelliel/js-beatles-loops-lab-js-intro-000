0fd0da1926cc35aedf2da3757654f74ec8cfaa54
Sam Belliel <sbelliel@gmail.com>
2018-07-27 01:52:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var array =[]
-  for ([i == 0]; [i < 4]; [i++]) {
+  var i = 0;
+  for ([i < 4]; [i++]) {
     array = musicians + "plays" + instruments
   }
   return array;
