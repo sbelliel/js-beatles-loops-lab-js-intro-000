163e018d12184c876d4980cb7e3162773e88a7c1
Sam Belliel <sbelliel@gmail.com>
2018-07-27 00:08:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
-  
+  for (i == 0; i < 4; i++) {
+    array[i] = musicians + "plays" + instruments;
+  }
+  return array;
 }
